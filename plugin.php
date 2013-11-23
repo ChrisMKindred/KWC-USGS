795eca783f0abc5e0b23e89583f8dd4746e5a037
Chris Kindred <Chris@kindredwebconsulting.com>
2013-11-23 00:08:26 -0700

    Updated admin page to use ajax and get all available stations for a state.

--- plugin.php
+++ plugin.php
@@ -28,112 +28,105 @@ License:
 */
 
 class KWCUSGS {
-
-	/*--------------------------------------------*
-	 * Constructor
-	 *--------------------------------------------*/
-
-	/**
-	 * Initializes the plugin by setting localization, filters, and administration functions.
-	 */
 	function __construct() {
 
-		// Load plugin text domain
 		add_action( 'init', array( $this, 'plugin_textdomain' ) );
-
-		// Register admin styles and scripts
 		add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
-
-		// Register site styles and scripts
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
 
-		// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
 		register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
 
-
 		add_action( 'admin_menu', array( $this, 'setmenu' ));
 		add_action( 'admin_init', array( $this, 'register_mysettings' ));
 
-	    /*
-	     * TODO:
-	     * Define the custom functionality for your plugin. The first parameter of the
-	     * add_action/add_filter calls are the hooks into which your code should fire.
-	     *
-	     * The second parameter is the function name located within this class. See the stubs
-	     * later in the file.
-	     *
-	     * For more information:
-	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
-	     */
-
-
 	    add_shortcode( "USGS", array( $this, 'USGS' ));
-	    
 	    add_filter( 'widget_text', 'do_shortcode' );
 
+		add_action( 'admin_footer', 'my_action_javascript' );
+		
+		add_action('wp_ajax_my_action', 'my_action_callback');
+
+		function my_action_callback() {
+			$state = $_POST['state'];
+
+			$url = "http://waterservices.usgs.gov/nwis/iv?stateCd=$state&format=waterml&parameterCd=00060";
+			$data = file_get_contents( $url );
+			if ( !$data ){
+		 		echo 'Error retrieving: ' . $url;
+		 		exit;
+			}
+		 	$data = str_replace( 'ns1:', '', $data );
+		  	// Load the XML returned into an object for easy parsing
+		  	$xml_tree = simplexml_load_string( $data );
+		  	if ( false === $xml_tree ){
+		  		echo 'Unable to parse USGS\'s XML';
+		  	  	exit;
+		  	}
+
+			$page = "<table class='widefat'>
+						<thead>
+						    <tr>
+						        <th></th>
+						        <th>Site Name</th>
+						        <th>Site Code</th>       
+						        <th>Latitude</th>
+						        <th>longitude<th>
+						    </tr>
+						</thead>
+						<tfoot>
+    						<tr>
+						        <th></th>
+						        <th>Site Name</th>
+						        <th>Site Code</th>       
+						        <th>Latitude</th>
+						        <th>longitude<th>
+							</tr>
+						</tfoot>";
+			foreach ($xml_tree->timeSeries as $site_data){	
+				$cnt = ++$cnt;
+  				$page .= "<tbody>
+							    <tr>
+							      	<td>". $cnt ."</td>
+							      	<td>". ucwords(strtolower($site_data->sourceInfo->siteName)) ."</td>
+							      	<td>". $site_data->sourceInfo->siteCode ."</td>
+							      	<td>". $site_data->sourceInfo->geoLocation->geogLocation->latitude ."</td>
+							      	<td>". $site_data->sourceInfo->geoLocation->geogLocation->longitude ."</td>
+							    </tr>";
+			}
+  			$page .= "</tbody></table>";
+  			echo $page;
+			die();
+		}	
 	} // end constructor
 
-	/**
-	 * Fired when the plugin is activated.
-	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
-	 */
 	public function activate( $network_wide ) {
-		// TODO:	Define activation functionality here
 	} // end activate
 
-	/**
-	 * Fired when the plugin is deactivated.
-	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
-	 */
 	public function deactivate( $network_wide ) {
-		// TODO:	Define deactivation functionality here
 	} // end deactivate
 
-	/**
-	 * Fired when the plugin is uninstalled.
-	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
-	 */
 	public function uninstall( $network_wide ) {
-		// TODO:	Define uninstall functionality here
 	} // end uninstall
 
-	/**
-	 * Loads the plugin text domain for translation
-	 */
 	public function plugin_textdomain() {
 		$domain = 'kwc-usgs-locale';
-		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+		$locale = apply_filters( 'plugin_locale' , get_locale(), $domain );
         load_textdomain( $domain, WP_LANG_DIR.'/'.$domain.'/'.$domain.'-'.$locale.'.mo' );
         load_plugin_textdomain( $domain, FALSE, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
 	} // end plugin_textdomain
 
-	/**
-	 * Registers and enqueues admin-specific styles.
-	 */
 	public function register_admin_styles() {
-
-		// TODO:	Change 'plugin-name' to the name of your plugin
 		wp_enqueue_style( 'kwc-usgs-admin-styles', plugins_url( 'kwcusgs/css/admin.css' ) );
-
 	} // end register_admin_styles
 
-	/**
-	 * Registers and enqueues admin-specific JavaScript.
-	 */
 	public function register_admin_scripts() {
-
-		// TODO:	Change 'plugin-name' to the name of your plugin
 		wp_enqueue_script( 'kwc-usgs-admin-script', plugins_url( 'kwcusgs/js/admin.js' ), array('jquery') );
 
 	} // end register_admin_scripts
-
 
 	public function register_mysettings() { // whitelist options
   		register_setting( 'myoption-group', 'new_option_name' );
@@ -141,38 +134,14 @@ class KWCUSGS {
   		register_setting( 'myoption-group', 'option_etc' );
 	}
 
-	/**
-	 * Registers and enqueues plugin-specific styles.
-	 */
 	public function register_plugin_styles() {
-
-		// TODO:	Change 'plugin-name' to the name of your plugin
 		wp_enqueue_style( 'kwc-usgs-plugin-styles', plugins_url( 'kwcusgs/css/display.css' ) );
-
 	} // end register_plugin_styles
 
-	/**
-	 * Registers and enqueues plugin-specific scripts.
-	 */
 	public function register_plugin_scripts() {
-
-		// TODO:	Change 'plugin-name' to the name of your plugin
 		wp_enqueue_script( 'kwc-usgs-plugin-script', plugins_url( 'kwcusgs/js/display.js' ), array('jquery') );
-
 	} // end register_plugin_scripts
 
-	/*--------------------------------------------*
-	 * Core Functions
-	 *---------------------------------------------*/
-
-	/**
- 	 * NOTE:  Actions are points in the execution of a page or process
-	 *        lifecycle that WordPress fires.
-	 *
-	 *		  WordPress Actions: http://codex.wordpress.org/Plugin_API#Actions
-	 *		  Action Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
-	 *
-	 */
 	function USGS($atts, $content = null ) {
 	   	extract( shortcode_atts( 
 	   		array(
@@ -183,19 +152,18 @@ class KWCUSGS {
 	
 		$url = "http://waterservices.usgs.gov/nwis/iv?site=$location&parameterCd=00010,00060,00065&format=waterml";
 		
-		$response = wp_remote_get($url);
+		$response = wp_remote_get( $url );
 	  	$data = wp_remote_retrieve_body( $response );
-	  	if (!$data){
+	  	
+	  	if ( ! $data ){
 	   		return 'USGS Not Responding.';
 	  	}
+
+		$data = str_replace( 'ns1:', '', $data );
+
+		$xml_tree = simplexml_load_string( $data );
 		
-		// Remove the namespace prefix for easier parsing
-		$data = str_replace('ns1:','', $data);
-		
-		// Load the XML returned into an object for easy parsing
-		$xml_tree = simplexml_load_string($data);
-		
-		if ($xml_tree === FALSE){
+		if ( False === $xml_tree ){
 			return 'Unable to parse USGS\'s XML';
 		}
 		if (!isset($title)){
@@ -209,15 +177,15 @@ class KWCUSGS {
 		$thePage = "<div class='KWC_USGS clearfix'>
 						<h3 class='header'>$SiteName</h3>
 							<ul class='sitevalues'>";
-	  	foreach ($xml_tree->timeSeries as $site_data){	
-	    	if ($site_data->values->value == '') {
+	  	foreach ( $xml_tree->timeSeries as $site_data ){	
+	    	if ( $site_data->values->value == '' ) {
 	      		$value = '-';
-	    	} else if ($site_data->values->value == -999999){
+	    	} else if ( $site_data->values->value == -999999 ){
 	      		$value = 'UNKNOWN';
 	      		$provisional = '-';
 	    	} else {
 	      		$desc   = $site_data->variable->variableName;
-	      		switch ($site_data->variable->variableCode) {
+	      		switch ( $site_data->variable->variableCode ) {
 	        		case "00010":
 	          			$value  = $site_data->values->value;
 	          			$degf   = (9/5) * $value + 32;          			       
@@ -225,15 +193,17 @@ class KWCUSGS {
 	          			$watertempdesc  = "&deg; F"; 
 	          			$thePage .= "<li class='watertemp'>Water Temp: $watertemp $watertempdesc</li>";
 	          			break;
+
 			        case "00060":
-			          	$splitDesc = explode(",",$desc);
+			          	$splitDesc = explode( ",", $desc );
 			          	$value  = $site_data->values->value;
 			          	$streamflow     = $value;
 			          	$streamflowdesc = $splitDesc[1];
 	          			$thePage .= "<li class='flow'>Flow: $streamflow $streamflowdesc</li>";
 			          	break;
+
 			        case "00065":
-			          	$splitDesc = explode(",",$desc);
+			          	$splitDesc = explode( ",", $desc );
 			          	$value  = $site_data->values->value;
 			          	$gageheight = $value;  
 			          	$gageheightdesc = $splitDesc[1];          
@@ -243,25 +213,17 @@ class KWCUSGS {
 	    	}
 	  	}
 		$thePage .=		"</ul>";
-		if (isset($graph)){
-			if ($graph == 'true'){
+		if ( isset( $graph ) ){
+			if ( true == $graph ){
 				$thePage .= "<img src='http://waterdata.usgs.gov/nwisweb/graph?site_no=$location&parm_cd=00060'/>";
 				$thePage .= "<img src='http://waterdata.usgs.gov/nwisweb/graph?site_no=$location&parm_cd=00065'/>";
 			}
 		}
-		$thePage .= "<a href='http://waterdata.usgs.gov/nwis/uv?$location'>USGS</a>";
+		$thePage .= "<a href='http://waterdata.usgs.gov/nwis/uv?$location' target='_blank'>USGS</a>";
 		$thePage .=	"</div>";
 	   	return $thePage;
 	}
 
-	/**
-	 * NOTE:  Filters are points of execution in which WordPress modifies data
-	 *        before saving it or sending it to the browser.
-	 *
-	 *		  WordPress Filters: http://codex.wordpress.org/Plugin_API#Filters
-	 *		  Filter Reference:  http://codex.wordpress.org/Plugin_API/Filter_Reference
-	 *
-	 */
 	function setmenu() {
 		add_submenu_page( 'options-general.php', 'Page title', 'USGS', 'manage_options', 'usgs', array( $this, 'my_magic_function' ) );
 	}
@@ -270,114 +232,121 @@ class KWCUSGS {
 		if ( !current_user_can( 'manage_options' ) )  {
 			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 		}
-$state_values=array(
-                'AL'=>"Alabama",
-                'AK'=>"Alaska",
-                'AZ'=>"Arizona",
-                'AR'=>"Arkansas",
-                'CA'=>"California",
-                'CO'=>"Colorado",
-                'CT'=>"Connecticut",
-                'DE'=>"Delaware",
-                'DC'=>"District Of Columbia",
-                'FL'=>"Florida",
-                'GA'=>"Georgia",
-                'HI'=>"Hawaii",
-                'ID'=>"Idaho",
-                'IL'=>"Illinois",
-                'IN'=>"Indiana",
-                'IA'=>"Iowa",
-                'KS'=>"Kansas",
-                'KY'=>"Kentucky",
-                'LA'=>"Louisiana",
-                'ME'=>"Maine",
-                'MD'=>"Maryland",
-                'MA'=>"Massachusetts",
-                'MI'=>"Michigan",
-                'MN'=>"Minnesota",
-                'MS'=>"Mississippi",
-                'MO'=>"Missouri",
-                'MT'=>"Montana",
-                'NE'=>"Nebraska",
-                'NV'=>"Nevada",
-                'NH'=>"New Hampshire",
-                'NJ'=>"New Jersey",
-                'NM'=>"New Mexico",
-                'NY'=>"New York",
-                'NC'=>"North Carolina",
-                'ND'=>"North Dakota",
-                'OH'=>"Ohio",
-                'OK'=>"Oklahoma",
-                'OR'=>"Oregon",
-                'PA'=>"Pennsylvania",
-                'RI'=>"Rhode Island",
-                'SC'=>"South Carolina",
-                'SD'=>"South Dakota",
-                'TN'=>"Tennessee",
-                'TX'=>"Texas",
-                'UT'=>"Utah",
-                'VT'=>"Vermont",
-                'VA'=>"Virginia",
-                'WA'=>"Washington",
-                'WV'=>"West Virginia",
-                'WI'=>"Wisconsin",
-                'WY'=>"Wyoming"
+		$state_values=array(
+                'AL' => "Alabama",
+                'AK' => "Alaska",
+                'AZ' => "Arizona",
+                'AR' => "Arkansas",
+                'CA' => "California",
+                'CO' => "Colorado",
+                'CT' => "Connecticut",
+                'DE' => "Delaware",
+                'DC' => "District Of Columbia",
+                'FL' => "Florida",
+                'GA' => "Georgia",
+                'HI' => "Hawaii",
+                'ID' => "Idaho",
+                'IL' => "Illinois",
+                'IN' => "Indiana",
+                'IA' => "Iowa",
+                'KS' => "Kansas",
+                'KY' => "Kentucky",
+                'LA' => "Louisiana",
+                'ME' => "Maine",
+                'MD' => "Maryland",
+                'MA' => "Massachusetts",
+                'MI' => "Michigan",
+                'MN' => "Minnesota",
+                'MS' => "Mississippi",
+                'MO' => "Missouri",
+                'MT' => "Montana",
+                'NE' => "Nebraska",
+                'NV' => "Nevada",
+                'NH' => "New Hampshire",
+                'NJ' => "New Jersey",
+                'NM' => "New Mexico",
+                'NY' => "New York",
+                'NC' => "North Carolina",
+                'ND' => "North Dakota",
+                'OH' => "Ohio",
+                'OK' => "Oklahoma",
+                'OR' => "Oregon",
+                'PA' => "Pennsylvania",
+                'RI' => "Rhode Island",
+                'SC' => "South Carolina",
+                'SD' => "South Dakota",
+                'TN' => "Tennessee",
+                'TX' => "Texas",
+                'UT' => "Utah",
+                'VT' => "Vermont",
+                'VA' => "Virginia",
+                'WA' => "Washington",
+                'WV' => "West Virginia",
+                'WI' => "Wisconsin",
+                'WY' => "Wyoming"
     );
-	function listUSStates($state_values,$dropdown_name,$key_selected) {
-	    $string="<select name=\"".$dropdown_name."\">\n";
-    	if (!empty($state_values)) {
-        	if ($key_selected=="" || !isset($key_selected)) {
-            	$string.="<option value=\"\"></option>\n";
+
+	function listUSStates( $state_values, $dropdown_name, $key_selected ) {
+	    $string = "<select id='state' class='state' name=\"" . $dropdown_name . "\">\n";
+    	if ( !empty( $state_values ) ) {
+        	if ( $key_selected == "" || !isset( $key_selected ) ) {
+            	$string .= "<option value=\"\"></option>\n";
         	}
-        	foreach($state_values as $state_short=>$state_full) {
-            	if ($key_selected!="" && $key_selected==$state_short) {
-                	$additional=" SELECTED";
+        	foreach( $state_values as $state_short=>$state_full ) {
+            	if ( $key_selected != "" && $key_selected == $state_short ) {
+                	$additional = " SELECTED";
+            	} else {
+                	$additional = "";
             	}
-            	else {
-                	$additional="";
-            	}
-                $string.="<option value=\"".$state_short."\"".$additional.">".$state_full."</option>\n";
+                $string.="<option value=\"" . $state_short . "\"" . $additional . ">" . $state_full . "</option>\n";
         	}
     	}
-    	$string.="</select>\n";
+    	$string .= "</select>\n";
     	return $string;
 	}
+	function my_action_javascript() {
+	?>
+		<script type="text/javascript" >
+		$j=jQuery.noConflict();
+		$j(document).ready(function() {
+			$j('.button-secondary').click(function() {
+				$j( "#results" ).html("Loading Stations...")
+				var data = {
+					action: 'my_action',
+					state: $j( ".state" ).val()
+				};
+
+			// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+				$j.post(ajaxurl, data, function(response) {
+					$j( "#results" ).html( response );
+				});
+  			});
+		});
+		</script>
+		<?php
+	}
+
 	?>
 		<div class="wrap">
 			<?php screen_icon(); ?> <h2>USGS Stream Flow Data</h2>
 			<h3>Get River ID</h3>
 			<label>Select State</label>
 			<?php 
-				$select_box_name = "roger";
+				$select_box_name = "state";
 				$key_selected = "";
-				echo listUSStates($state_values,$select_box_name,$key_selected);
-//			$url = 'http://waterservices.usgs.gov/nwis/iv?stateCd=CO&format=waterml&parameterCd=00060';
-//			$data = file_get_contents($url);
-//			if (!$data){
-//			 	echo 'Error retrieving: ' . $url;
-//			 	exit;
-//			}
-//			// Remove the namespace prefix for easier parsing
-//			$data = str_replace('ns1:','', $data);
-//			// Load the XML returned into an object for easy parsing
-//			$xml_tree = simplexml_load_string($data);
-//			if ($xml_tree === FALSE){
-//			 	echo 'Unable to parse USGS\'s XML';
-//			  exit;
-//			}
-//			echo "<pre>";
-//			print_r($xml_tree);
-//			echo "</pre>";
+				echo listUSStates( $state_values, $select_box_name, $key_selected );
 			?>
+			<input type='submit' value='<?php _e('Get Stations'); ?>' class='button-secondary' />
+	    	<br /><br />
+
+	    	<div class="" id="results"></div>
 	    </div>
 	    <?php 	
 	}
 
 	function filter_method_name() {
-	    // TODO:	Define your filter method here
 	} // end filter_method_name
 
 } // end class
 
-// TODO:	Update the instantiation call of your plugin to the name given at the class definition
 $KWCUSGS = new KWCUSGS();
