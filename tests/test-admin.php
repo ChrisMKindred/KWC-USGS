12cc60462db0a453afcf416ccf76fa7cb2a8b506
Chris Kindred <gator783@gmail.com>
2018-10-24 09:25:55 -0400

    Adds Tests for Admin add_plugin_admin_menu (#17)
    Adds Tests for Admin `add_plugin_admin_menu()`, `add_action_list()`

--- tests/test-admin.php
+++ tests/test-admin.php
@@ -64,4 +64,32 @@ class AdminTests extends WP_UnitTestCase {
 		$this->instance->enqueue_admin_scripts();
 		$this->assertTrue( wp_script_is( 'kwcusgs-admin-script' ) );
 	}
+
+	/**
+	 * Does the options page actually exists for the admin user.
+	 *
+	 * @covers Kwc_Usgs_Admin::add_plugin_admin_menu
+	 */
+	public function test_admin_add_plugin_admin_menu() {
+		wp_set_current_user(
+			self::factory()->user->create(
+				array(
+            		'role' => 'administrator',
+				)
+			)
+		);
+		$this->instance->add_plugin_admin_menu();
+		$this->assertNotEmpty( menu_page_url( 'kwcusgs', false ) );
+	}
+
+	/**
+	 * Does the array for the settings link work
+	 *
+	 * @covers Kwc_Usgs_Admin::add_action_links
+	 */
+	public function test_admin_add_action_links() {
+		$actions = array();
+		$actions = $this->instance->add_action_links( $actions );
+		$this->assertNotEmpty( $actions['settings'] );
+	}
 }
